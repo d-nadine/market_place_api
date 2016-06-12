13bec5446100900e805a34a7e07c2d647fe99df4
davidnowak0717 <freshfriend_0717@aol.com>
2016-06-12 12:48:48 -0700

    Upload

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+require 'api_constraints'
+
+Rails.application.routes.draw do
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+    end
+  end
+end
